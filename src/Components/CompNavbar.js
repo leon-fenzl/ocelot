0bfd6f96cb62f85835a3c4cf15bfef9404ab9aa6
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-02-16 11:43:30 -0300

    Update ocelot

---
+++ src/Components/CompNavbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import "../Styles/navbar.css"
+export default function CompNavbar(){
+  return(
+    <div className="custom-navbar">
+      <div className="LOGO">
+        <Link to="/ocelot" className="link"><h1><p>León Fenzl</p></h1></Link>
+        <h0><p>3D Artist - Programmer</p></h0>
+      </div>
+      <div className="flex-menu">
+        <div className="mobileMenu">
+          <Link className="link"><h2>Mobile Menu</h2></Link>
+        </div>
+        <div className="link-page">
+          <Link to="/portfolio" className="link"><h2>Portfolio</h2></Link>
+        </div>
+        <div className="link-page">
+          <Link to="/curricullum" className="link"><h2>Curriculum</h2></Link>
+        </div>
+        <div className="link-page">
+          <Link to="/tutorial" className="link"><h2>Tutorials</h2></Link>
+        </div>
+      </div>
+    </div>
+  );
+}
